e676a0a191c1328e950f725cf65badbbe0d05d5e
Mauricio Robayo <rfmajo@gmail.com>
2023-11-08 15:24:34 -0500

    styles

--- src/app/GoogleMap.tsx
+++ src/app/GoogleMap.tsx
@@ -5,10 +5,12 @@ import { APIProvider, Map } from "@vis.gl/react-google-maps";
 export default function GoogleMap({ zoom }: { readonly zoom: number }) {
   const position = { lat: 53.54, lng: 10 };
 
+  console.log({ zoom });
+
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? ""}>
-      <div className="aspect-video">
-        <Map zoom={zoom} center={position} />
+      <div className="aspect-video max-w-lg">
+        <Map zoom={zoom} center={position} disableDefaultUI />
       </div>
     </APIProvider>
   );
